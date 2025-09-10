68d60574dba304f28307936b56a8ff0d669a1413
hourin <phucjaki@gmail.com>
2025-09-10 22:57:45 +0700

    Added: models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Artwork(models.Model):
+    title = models.CharField(max_length=200);
+    image = models.ImageField(upload_to="artworks/");
+    uploadt = models.DateTimeField(auto_now_add=True);
+    # TODO: tag and uploader foreign key
+
+class Tag(models.Model):
+    class Type(models.IntegerChoices):
+        ARTIST = 1;
+        CHARACTER = 2;
+        SERIES = 3;
+
+    name = models.CharField(max_length=50);
+    ttype = models.IntegerField(choices=Type);
+
